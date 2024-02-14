46323ed52cb3c17116fa61c321ebfcd7a16b5b87
ASUS\natta <nattavutkhuntamli@gmail.com>
2024-02-14 21:50:52 +0700

    no message

--- api/routes/Admin.js
+++ api/routes/Admin.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { query , check, validationResult } from 'express-validator';
+import { query , param,check, validationResult } from 'express-validator';
 import AdminController from '../controllers/Admin.js'
 import Auth_admin from '../middleware/Auth_admin.js';
 const router = express.Router();
@@ -81,5 +81,47 @@ router.post('/login',[
     }
 })
 
+
+router.patch('/editProfile/:id', Auth_admin, [
+    param('id','ระบุรหัสของ id').not().isEmpty(),
+    check('fullname', 'Fullname is required').not().isEmpty(),
+    check('email', 'Email is required').not().isEmpty(),
+    check('phone', 'Phone is required').not().isEmpty(),
+    check('address', 'Address is required').not().isEmpty()
+], async(req,res) => {
+    try{
+        const ErrorsValidation = validationResult(req);
+        if (!ErrorsValidation.isEmpty()) {
+            const ErrorMsg = ErrorsValidation.array().map((err) => err.msg);
+            return res.status(400).json({ message: ErrorMsg });
+        }
+        const updateProfile =  await AdminController.updateProfile({id: req.params.id, fullname: req.body.fullname, email: req.body.email, phone: req.body.phone, address: req.body.address});
+        return res.status(200).json({
+            message: 'Successfully updated profile',
+        })
+
+    } catch (error) {
+        return res.status(error.statusCode || 500).json({ error: error.message });
+    }
+})
+
+router.patch('/editPassword/:id',Auth_admin,[
+    param('id','ระบุรหัสของ id').not().isEmpty(),
+    check('password', '<PASSWORD>').not().isEmpty()
+], async(req,res) => {
+    try {
+        const ErrorsValidation = validationResult(req);
+        if (!ErrorsValidation.isEmpty()) {
+            const ErrorMsg = ErrorsValidation.array().map((err) => err.msg);
+            return res.status(400).json({ message: ErrorMsg });
+        }
+        const updatePassword =  await AdminController.updatePassword({id: req.params.id, password: req.body.password});
+        return res.status(200).json({
+            message: 'Successfully updated password',
+        })
+    } catch (error) {
+        return res.status(error.statusCode || 500).json({ error: error.message });
+    }
+})
 
 export default router;
